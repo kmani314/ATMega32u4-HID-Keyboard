110bedde03697975545f44d944b779ad7a646ead
kmani314 <krishnamani314@gmail.com>
2019-08-16 21:15:25 -0700

    Update Vendor IDs

--- include/usb.h
+++ include/usb.h
@@ -31,9 +31,8 @@ int send_keypress(uint8_t, uint8_t);
 #define GET_INTERFACE 0x0A
 #define SET_INTERFACE 0x0B
 
-#define idVendor 0x258a  // Atmel Corp.
-#define idProduct \
-  0x1006  // ATMega32u4 DFU Bootloader (This isn't a real product so I don't
+#define idVendor 0x03eb  // Atmel Corp.
+#define idProduct 0x2ff4  // ATMega32u4 DFU Bootloader (This isn't a real product so I don't
           // have legitimate IDs)
 #define KEYBOARD_ENDPOINT_NUM 3  // The second endpoint is the HID endpoint
 
